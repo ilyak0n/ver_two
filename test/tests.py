343bb176c5127af46005849f245ed1bd7d88b03f
ilyak0n <144427269+ilyak0n@users.noreply.github.com>
2024-03-12 21:29:57 +0300

    creating repository

---
+++ test/tests.py
@@ -0,0 +1,16 @@
+nums = 0
+a = []
+b = []
+while nums <= 1:
+    print("Список не должен быть единичным!")
+    nums = int(input("Введите количество элементов: "))
+else:
+    print("Вводите числа по не убыванию!")
+    a.append(int(input()))
+    while len(a) != nums:
+        num = int(input())
+        if a[-1] <= num:
+            a.append(num)
+        else:
+            print('Вы ввели число, меньшее предыдущего!')
+    print(len(set(a)))
